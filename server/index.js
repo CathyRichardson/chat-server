fdb45da6f98553f710393946f46291adc56994bd
Cathy Richardson <cathyanne08@gmail.com>
2021-07-08 21:16:03 -0600

    on #6

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/api/messages', messages_controller.readMessage);
-app.post('/api/messages', messages_controller.createMessage);
-app.put('/api/messages/:id', messages_controller.updateMessage);
-app.delete('/api/messages/:id', messages_controller.deleteMessage);
+const messagesBaseUrl = "/api/messages"
+app.get('messagesBaseUrl', messages_controller.readMessage);
+app.post('messagesBaseUrl', messages_controller.createMessage);
+app.put(`${messagesBaseUrl}/:id`, messages_controller.updateMessage);
+app.delete(`${messagesBaseUrl}/:id`, messages_controller.deleteMessage);
 
 const port = 3001;
 app.listen(port, () => console.log(`listening on port ${port}`));
