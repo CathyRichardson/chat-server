20235666567462c4cb7dda44cdf839b512cda5d4
Cathy Richardson <cathyanne08@gmail.com>
2021-07-08 21:01:56 -0600

    added the functions to #4

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,16 +1,17 @@
 let messageArr = [];
-
-let id =0;
+let id = 0;
 
 const createMessage = (req, res) => {
-    const {text, time} = req.body
+    const { text, time } = req.body
+    // messageArr.push({ id, text, time });
     let newMessage = {
         text,
         time,
         id
     }
+    messageArr.push(newMessage);
     id++;
-    res.status(200).send(messageArr); 
+    res.status(200).send(messageArr);
 }
 
 const readMessage = (req, res) => {
@@ -18,10 +19,23 @@ const readMessage = (req, res) => {
 }
 
 const updateMessage = (req, res) => {
+    const updateID = req.params.id;
+    const updateText = req.body.text;
+    const index = messageArr.findIndex(e => e.id == updateID);
+    messageArr[index].text = updateText
+     // let message = messageArr[index];
+    // messageArr[index] = {
+    //   id: message.id,
+    //   text: text || message.text,
+    //   time: message.time
+    // };
     res.status(200).send(messageArr);
 }
 
 const deleteMessage = (req, res) => {
+    const deleteID = req.params.id;
+    const index = messageArr.findIndex(e => e.id == deleteID);
+    messageArr.splice(index, 1);
     res.status(200).send(messageArr);
 }
 
--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.use(express.json());
 
-app.get(url, messages_controller.readMessage);
-app.post(url, messages_controller.createMessage);
-app.put(url, messages_controller.updateMessage);
-app.delete(url, messages_controller.deleteMessage);
+app.get('/api/messages', messages_controller.readMessage);
+app.post('/api/messages', messages_controller.createMessage);
+app.put('/api/messages/:id', messages_controller.updateMessage);
+app.delete('/api/messages/:id', messages_controller.deleteMessage);
 
 const port = 3001;
 app.listen(port, () => console.log(`listening on port ${port}`));
